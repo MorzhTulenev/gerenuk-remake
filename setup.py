a41d56a9a5cc600cadb7b5a6b188c44f5110b575
MorzhTulenev <sembornick@gmail.com>
2026-09-19 01:54:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-#! /usr/bin/env python
-
-#from distutils.core import setup
 from setuptools import setup
 
 setup(
-    name="gerenuk",
-    version="0.1.0",
-    author="Jeet Sukumaran",
+    name="gerenuk-reloaded",
+    version="0.2.0",
+    author="Jeet Sukumaran, Morzh_Tulenev",
     author_email="jeetsukumaran@gmail.com",
     packages=["gerenuk"],
     scripts=[
         "bin/gerenuk-simulate.py",
         "bin/gerenuk-reject.py",
-        ],
-    url="http://pypi.python.org/pypi/gerenuk/",
-    test_suite = "gerenuk.test",
-    license="LICENSE.txt",
-    description="A Project",
-    long_description=open("README.txt").read(),
-    # install_requires=[ ],
+    ],
+    url="https://github.com/yourname/gerenuk-reloaded",
+    license="MIT",
+    description="Fork of gerenuk",
+    long_description=open("README.txt", encoding="utf-8").read(),
+    long_description_content_type="text/plain",
+    python_requires=">=3.9",
+    install_requires=[],
+    extras_require={
+        "test": ["pytest>=7"],
+    },
 )
